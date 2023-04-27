70d69eea7379d0beafdfeef8f4a8ffd2f2a52341
mirlau <miranda.lauri@gmail.com>
2023-04-27 18:22:28 +0100

    Checking output

--- main.js
+++ main.js
@@ -4,17 +4,19 @@ const result = document.getElementById('result');
 const restart = document.getElementById('restart');
 const modal = document.querySelector('.modal');
 const scoreboard = {
-    player: 0,
-    computer: 0
+  player: 0,
+  computer: 0
 };
 
  // Play game
- function play(e) {
-    restart.style.display = 'inline-block';
-    const playerChoice = e.target.id;
-    const computerChoice = getComputerChoice();
-    const winner = getWinner (playerChoice, computerChoice);
- }
+function play(e) {
+  restart.style.display = 'inline-block';
+  const playerChoice = e.target.id;
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(playerChoice, computerChoice);
+  
+  console.log(playerChoice, computerChoice, winner)
+    }
 
 // Gets computes choice
 function getComputerChoice() {
@@ -26,31 +28,32 @@ function getComputerChoice() {
     } else {
         return 'scissors';
     }
+    }
 
     // Get game winner
-    function getWinner (player, computer) {
-        if (player == computer) {
-            return 'draw';
-        } else if(player === 'rock') {
-            if(computer === 'paper') {
-            return 'computer';
-        } else {
-            return 'player';  
-        }
-     } else if(player === 'paper') {
-        if(computer === 'scissors') {
-            return 'computer';
-        } else {
-            return 'player';
-        }
-        } else if(player === 'scissors'){
-            if(computer === 'rock') {
-                return 'computer'
-            } else{
-                return 'player'
-            }
-        }
+    function getWinner(player, computer) {
+    if (player === computer) {
+      return 'draw';
+    } else if (player === 'rock') {
+      if (computer === 'paper') {
+        return 'computer';
+      } else {
+        return 'player';
+      }
+    } else if (player === 'paper') {
+      if (computer === 'scissors') {
+        return 'computer';
+      } else {
+        return 'player';
+      }
+    } else if (player === 'scissors') {
+      if (computer === 'rock') {
+        return 'computer';
+      } else {
+        return 'player';
+      }
     }
+  }
 
  // Event listeners
  choices.forEach(choice => choice.addEventListener('click', play));
